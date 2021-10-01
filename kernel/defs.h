afd484749b509321d41531e24d6ea1442f0a7748
mannkafai <mannkafai@users.noreply.github.com>
2021-10-01 18:15:08 +0800

    Lazy allocation

--- kernel/defs.h
+++ kernel/defs.h
@@ -171,6 +171,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+int             proc_pagefault(pagetable_t, uint64 va, uint64* pa);
 
 // plic.c
 void            plicinit(void);
--- kernel/vm.c
+++ kernel/vm.c
@@ -181,9 +181,9 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      continue; // panic("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      continue; // panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -439,4 +439,24 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+int 
+proc_pagefault(pagetable_t pagetable, uint64 va, uint64* pa)
+{
+  char *mem;
+  uint64 a = PGROUNDDOWN(va);
+  
+  mem = kalloc();
+  if (mem == 0)
+    return -1;
+
+  memset(mem, 0, PGSIZE);
+  if (mappages(pagetable, a, PGSIZE, (uint64)mem, PTE_W | PTE_X | PTE_R | PTE_U) != 0)
+  {
+    kfree(mem);
+    return -1;
+  }
+  *pa = (uint64)mem;
+  return 0;
 }
